b8f03b44f46cc7ac3eb93af3adc3948c120733ff
Eduard Bopp <eduard.bopp@aepsil0n.de>
2014-09-17 15:01:55 +0200

    Multiple cubes

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,13 @@ use std::iter::Range;
 #[vertex_format]
 struct Vertex {
     #[as_float]
-    a_pos: [i8, ..3],
+    a_pos: [f32, ..3],
     #[as_float]
-    a_tex_coord: [u8, ..2],
+    a_tex_coord: [f32, ..2],
 }
 
 impl Vertex {
-    fn new(pos: [i8, ..3], tc: [u8, ..2]) -> Vertex {
+    fn new(pos: [f32, ..3], tc: [f32, ..2]) -> Vertex {
         Vertex {
             a_pos: pos,
             a_tex_coord: tc,
@@ -171,53 +171,79 @@ fn main() {
     let (mut device, frame) = window.gfx();
     let state = gfx::DrawState::new().depth(gfx::state::LessEqual, true);
 
-    let vertex_data = vec![
-        //top (0, 0, 1)
-        Vertex::new([-1, -1,  1], [0, 0]),
-        Vertex::new([ 1, -1,  1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([-1,  1,  1], [0, 1]),
-        //bottom (0, 0, -1)
-        Vertex::new([ 1,  1, -1], [0, 0]),
-        Vertex::new([-1,  1, -1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([ 1, -1, -1], [0, 1]),
-        //right (1, 0, 0)
-        Vertex::new([ 1, -1, -1], [0, 0]),
-        Vertex::new([ 1,  1, -1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([ 1, -1,  1], [0, 1]),
-        //left (-1, 0, 0)
-        Vertex::new([-1,  1,  1], [0, 0]),
-        Vertex::new([-1, -1,  1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([-1,  1, -1], [0, 1]),
-        //front (0, 1, 0)
-        Vertex::new([-1,  1, -1], [0, 0]),
-        Vertex::new([ 1,  1, -1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([-1,  1,  1], [0, 1]),
-        //back (0, -1, 0)
-        Vertex::new([ 1, -1,  1], [0, 0]),
-        Vertex::new([-1, -1,  1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([ 1, -1, -1], [0, 1]),
+    let basic_cube = vec![
+        //top (0., 0., 1.)
+        Vertex::new([-1., -1.,  1.], [0., 0.]),
+        Vertex::new([ 1., -1.,  1.], [1., 0.]),
+        Vertex::new([ 1.,  1.,  1.], [1., 1.]),
+        Vertex::new([-1.,  1.,  1.], [0., 1.]),
+        //bottom (0., 0., -1.)
+        Vertex::new([ 1.,  1., -1.], [0., 0.]),
+        Vertex::new([-1.,  1., -1.], [1., 0.]),
+        Vertex::new([-1., -1., -1.], [1., 1.]),
+        Vertex::new([ 1., -1., -1.], [0., 1.]),
+        //right (1., 0., 0.)
+        Vertex::new([ 1., -1., -1.], [0., 0.]),
+        Vertex::new([ 1.,  1., -1.], [1., 0.]),
+        Vertex::new([ 1.,  1.,  1.], [1., 1.]),
+        Vertex::new([ 1., -1.,  1.], [0., 1.]),
+        //left (-1., 0., 0.)
+        Vertex::new([-1.,  1.,  1.], [0., 0.]),
+        Vertex::new([-1., -1.,  1.], [1., 0.]),
+        Vertex::new([-1., -1., -1.], [1., 1.]),
+        Vertex::new([-1.,  1., -1.], [0., 1.]),
+        //front (0., 1., 0.)
+        Vertex::new([-1.,  1., -1.], [0., 0.]),
+        Vertex::new([ 1.,  1., -1.], [1., 0.]),
+        Vertex::new([ 1.,  1.,  1.], [1., 1.]),
+        Vertex::new([-1.,  1.,  1.], [0., 1.]),
+        //back (0., -1., 0.)
+        Vertex::new([ 1., -1.,  1.], [0., 0.]),
+        Vertex::new([-1., -1.,  1.], [1., 0.]),
+        Vertex::new([-1., -1., -1.], [1., 1.]),
+        Vertex::new([ 1., -1., -1.], [0., 1.]),
     ];
+    let vertex_data = {
+        let mut data = Vec::new();
+        for i in range(0u, 10) {
+            data.extend(
+                basic_cube.iter()
+                    .map(|v| {
+                        Vertex::new(
+                            [
+                                v.a_pos[0] * 0.5 + (i as f32),
+                                v.a_pos[1] * 0.5,
+                                v.a_pos[2] * 0.5,
+                            ],
+                            v.a_tex_coord
+                        )
+                    })
+            );
+        }
+        data
+    };
 
     let mesh = device.create_mesh(vertex_data);
 
     let slice = {
-        let index_data = vec![
-            0u8, 1, 2, 2, 3, 0,    //top
-            4, 5, 6, 6, 7, 4,       //bottom
-            8, 9, 10, 10, 11, 8,    //right
-            12, 13, 14, 14, 16, 12, //left
-            16, 17, 18, 18, 19, 16, //front
-            20, 21, 22, 22, 23, 20, //back
-        ];
+        let mut index_data = Vec::new();
+        for i in range(0u32, 10) {
+            index_data.extend(
+                vec![
+                    0, 1, 2, 2, 3, 0,    //top
+                    4, 5, 6, 6, 7, 4,       //bottom
+                    8, 9, 10, 10, 11, 8,    //right
+                    12, 13, 14, 14, 16, 12, //left
+                    16, 17, 18, 18, 19, 16, //front
+                    20, 21, 22, 22, 23, 20, //back
+                ]
+                .iter()
+                .map(|&index| index + (i * 24))
+            );
+        }
 
         let buf = device.create_buffer_static(&index_data);
-        gfx::IndexSlice8(gfx::TriangleList, buf, 0, index_data.len() as u32)
+        gfx::IndexSlice32(gfx::TriangleList, buf, 0, index_data.len() as u32)
     };
 
     let tinfo = gfx::tex::TextureInfo {
