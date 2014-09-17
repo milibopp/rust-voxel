c32d58de28e59e45589c8e6f91a50fe8d41453bb
Eduard Bopp <eduard.bopp@aepsil0n.de>
2014-09-17 15:14:01 +0200

    Factor out basic cube indexing

--- src/main.rs
+++ src/main.rs
@@ -225,20 +225,21 @@ fn main() {
 
     let mesh = device.create_mesh(vertex_data);
 
+    let cube_indices = vec![
+        0, 1, 2, 2, 3, 0,    //top
+        4, 5, 6, 6, 7, 4,       //bottom
+        8, 9, 10, 10, 11, 8,    //right
+        12, 13, 14, 14, 16, 12, //left
+        16, 17, 18, 18, 19, 16, //front
+        20, 21, 22, 22, 23, 20, //back
+    ];
+
     let slice = {
         let mut index_data = Vec::new();
         for i in range(0u32, 10) {
             index_data.extend(
-                vec![
-                    0, 1, 2, 2, 3, 0,    //top
-                    4, 5, 6, 6, 7, 4,       //bottom
-                    8, 9, 10, 10, 11, 8,    //right
-                    12, 13, 14, 14, 16, 12, //left
-                    16, 17, 18, 18, 19, 16, //front
-                    20, 21, 22, 22, 23, 20, //back
-                ]
-                .iter()
-                .map(|&index| index + (i * 24))
+                cube_indices.iter()
+                .map(|&index| index + (i * basic_cube.len() as u32))
             );
         }
 
